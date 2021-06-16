37b2ea1e49bc610b83d0cd5453403a188fd7b324
wellington barbosa <carneirobarbosawellington@gmail.com>
2021-06-16 14:53:16 -0300

    Created Author Model

---
+++ app/Models/Author.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Author extends Model
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'gender',
+        'country'
+    ];
+}
