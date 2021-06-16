1f0367f3630426ba10bd7fe43d4670aa99ab146c
wellington barbosa <carneirobarbosawellington@gmail.com>
2021-06-16 15:18:33 -0300

    Generated base author controller

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -2,17 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
+use Illuminate\Http\Request;
+
 class AuthorsController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Display a listing of the resource.
      *
-     * @return void
+     * @return \Illuminate\Http\Response
      */
-    public function __construct()
+    public function index()
     {
         //
     }
 
-    //
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
 }
